1adb0498d0aa2d82c5b5e46436f82db941158f77
Xardas <vadikradik@yandex.ru>
2023-01-26 17:58:14 +0300

    get user action

--- src/components/login-top-block/login-top-block.tsx
+++ src/components/login-top-block/login-top-block.tsx
@@ -1,10 +1,25 @@
-/* eslint-disable import/named */
 import { Button } from 'antd'
+// eslint-disable-next-line import/named
 import { RouteComponentProps, withRouter } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { getUser, RootState } from '../../services/store/user-slice'
+import { AppDispatch } from '../../services/store/store'
 
 import classes from './login-top-block.module.scss'
 
 const LoginTopBlock = (props: RouteComponentProps) => {
+  const userState = useSelector((state: RootState) => state.users)
+  const dispatch = useDispatch<AppDispatch>()
+  useEffect(() => {
+    console.log('header useeffect')
+    if (!userState.isLoggedIn && window.localStorage.getItem('auth_token')) {
+      console.log('gettting user')
+      dispatch(getUser())
+    }
+  }, [])
+
   return (
     <div className={classes['login-top-block']}>
       <Button
--- src/services/store/user-slice.tsx
+++ src/services/store/user-slice.tsx
@@ -59,6 +59,32 @@ export const postUser = createAsyncThunk<PostUserResponse, UserCredentials, { re
   },
 )
 
+export const getUser = createAsyncThunk<PostUserResponse, void, { rejectValue: KnownError }>(
+  'user/getUser',
+  async (_, { rejectWithValue }) => {
+    console.log('request sent')
+    const response = await fetch('https://blog.kata.academy/api/user', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization: `Bearer ${window.localStorage.getItem('auth_token')}`,
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
+        } else {
+          return rejectWithValue({ message: `Unable to get user, responce status: ${res.status}` })
+        }
+      })
+      .catch((error) => {
+        return rejectWithValue({ message: `Unable to get user, error: ${error.message}` })
+      })
+
+    return response
+  },
+)
+
 interface UserState {
   email: string
   username: string
@@ -66,6 +92,7 @@ interface UserState {
   image: string
   loading: boolean
   error: KnownError | null
+  isLoggedIn: boolean
 }
 
 export type RootState = {
@@ -79,14 +106,21 @@ const initialState: UserState = {
   image: '',
   loading: false,
   error: null,
+  isLoggedIn: false,
 }
 
 export const usersSlice = createSlice({
   name: 'users',
   initialState,
-  reducers: {},
+  reducers: {
+    logOut(state) {
+      state.isLoggedIn = false
+      window.localStorage.setItem('auth_token', '')
+    },
+  },
   extraReducers: (builder) => {
     builder
+      // postUser
       .addCase(postUser.pending, (state) => {
         state.loading = true
         state.error = null
@@ -106,7 +140,33 @@ export const usersSlice = createSlice({
         state.loading = false
         state.error = action.payload ?? null
       })
+      // getUser
+      .addCase(getUser.pending, (state) => {
+        state.loading = true
+        state.error = null
+      })
+      .addCase(getUser.fulfilled, (state, action) => {
+        console.log(action)
+        state.loading = false
+        state.email = action.payload.user.email
+        state.username = action.payload.user.username
+        state.bio = action.payload.user.bio
+        state.image = action.payload.user.image
+        state.error = null
+        state.isLoggedIn = true
+        console.log('logged in')
+      })
+      .addCase(getUser.rejected, (state, action) => {
+        console.log(action)
+        state.loading = false
+        state.error = action.payload ?? null
+        state.isLoggedIn = false
+        window.localStorage.setItem('auth_token', '')
+        console.log('logged out')
+      })
   },
 })
+
+export const { logOut } = usersSlice.actions
 
 export default usersSlice.reducer
